d3bcca7641081dfe239ec0505b77365ffa42f1ef
Chris Eddy <ceddy.zen@gmail.com>
2019-01-28 09:54:34 -0800

    initial UI

---
+++ ICS224-MobileApps/lab2/TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Drake on 2019-01-28.
+//  Copyright © 2019 Drake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

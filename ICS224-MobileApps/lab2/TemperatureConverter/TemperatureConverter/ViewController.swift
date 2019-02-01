b084e36ecddfe0827be9b105f7c648ee90023fe9
Chris Eddy <ceddy.zen@gmail.com>
2019-01-31 21:22:29 -0800

    checkpoint code

--- ICS224-MobileApps/lab2/TemperatureConverter/TemperatureConverter/ViewController.swift
+++ ICS224-MobileApps/lab2/TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var temperature:Double = 0
+    
+    // MARK: Properties
+    @IBOutlet weak var tempField: UITextField!
+    @IBOutlet weak var tempResult: UILabel!
+    
+    // MARK: DelegateMethods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tempField.delegate = self
+    }
+    func textFieldShouldReturn(_textField: UITextField) -> Bool{
+        self.tempField.resignFirstResponder()
+        return true
     }
 
-
+    // MARK: Actions
+    @IBAction func toF(_ sender: Any) {
+        let input:String = self.tempField.text!
+        temperature = Double(input)! * 9.0 / 5.0 + 32.0
+        self.tempResult.text = String(temperature)
+    }
+    
+    @IBAction func toC(_ sender: Any) {
+        let input:String = self.tempField.text!
+        temperature = Double(input)! * 5.0 / 9.0
+        self.tempResult.text = String(temperature)
+    }
 }
 

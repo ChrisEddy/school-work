d98f85af6f2c200ff4de36453036fd3d330a94f3
Chris Eddy <ceddy.zen@gmail.com>
2019-02-06 20:44:38 -0800

    lab2 web apps

--- ICS221-WebServices/lab2/ics221-mb/app_server/components/msgList.jsx
+++ ICS221-WebServices/lab2/ics221-mb/app_server/components/msgList.jsx
@@ -3,7 +3,23 @@ const React = require('react');
 const MsgList = (props) => {
     return(
         <div className="MsgList">
-        
+            <table className="table table-striped table-bordered">
+                <tr>
+                    <th className="w-25"></th>
+                    <th className="w-25"></th>
+                    <th className="w-50"></th>
+                </tr>
+                <tbody>
+                {props.messages.sort((a, b) => a.id - b.id).
+                reverse().map( (message, index) =>
+                <tr key={message.id}>
+                    <td>{index+1}</td>
+                    <td>{message.name}</td>
+                    <td>{message.msg}</td>
+                </tr>
+                )}
+                </tbody>
+            </table>
         </div>
     )
 }

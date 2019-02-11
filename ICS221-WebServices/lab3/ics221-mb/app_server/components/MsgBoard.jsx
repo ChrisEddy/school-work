946c02954cd103bc07e9885b41792b181d305013
Chris Eddy <chris.eddy.dev@gmail.com>
2019-02-11 13:04:23 -0800

    lab 3 work, web services

--- ICS221-WebServices/lab3/ics221-mb/app_server/components/MsgBoard.jsx
+++ ICS221-WebServices/lab3/ics221-mb/app_server/components/MsgBoard.jsx
@@ -1,20 +1,64 @@
 const React = require('react');
 const MsgList = require('./MsgList');
-
+const NewMsg = require('./NewMsg.jsx');
 
 class MsgBoard extends React.Component{
     constructor(props) {
-        
         super(props);
-            this.state = {
-                messages: this.props.messages
-            };
+        this.state = {
+            messages: this.props.messages
+        };
+        this.addMessage = this.addMessage.bind(this);
+    }
 
+    componentDidMount(){
+        console.log('hello')
+        fetch('http://localhost:3003/msgs', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function(e) {
+            console.log(e);
+        });
+    }
+
+    addMessage(message) {
+        let msgs = this.state.messages;
+     
+        // add id attribute
+        message.id = msgs.length;
+        // append to array
+        msgs.push(message);
+        // update state var
+        this.setState({
+          messages: msgs
+        });
+     
+        // update back-end data
+        fetch('http://localhost:3003/msgs', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(message)
+        })
+        .then(response=> this.handleHTTPErrors(response))
+        .catch(error=> {
+          console.log(error);
+        });
     }
 
     render() {
         return (
-            <MsgList messages={this.state.messages}/>
+            <div>
+                <NewMsg addMsgCallback={this.addMessage} />
+                <MsgList messages={this.state.messages}/>
+            </div>
         )
     }
 }
